bc04c75b7b3b07d2e2cc6bb6bcb01fe3c474ed49
Eddy Alburquerque <cordtripper@users.noreply.github.com>
2015-10-07 11:03:15 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ ! -f /var/lib/unifi/data/keystore ]]; then
+if [[ ! -f /var/lib/unifialpha/data/keystore ]]; then
     keytool -genkey -keyalg RSA -alias unifi -keystore /var/lib/unifialpha/data/keystore -storepass aircontrolenterprise -keypass aircontrolenterprise -validity 1825 -keysize 4096 -dname "cn=unfi"
 fi
 
-exec /usr/lib/jvm/java-8-oracle/jre/bin/java -Xmx1024M -jar /usr/lib/unifialpha/lib/ace.jar start
+exec /usr/lib/jvm/java-8-oracle/jre/bin/java -Xmx1024M -jar /var/lib/unifialpha/lib/ace.jar start
